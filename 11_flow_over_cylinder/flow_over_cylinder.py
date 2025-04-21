80081de375051c167c34f1b486a18925bc8f29eb
jijoderick <derickjijo@gmail.com>
2025-04-21 14:31:32 +0530

    Readme file

--- 11_flow_over_cylinder/flow_over_cylinder.py
+++ 11_flow_over_cylinder/flow_over_cylinder.py
@@ -2,6 +2,9 @@ from fenics import *
 import numpy as np
 from mshr import *
 
+import matplotlib.pyplot as plt
+# from dolfin import *
+
 # Parameters
 U_in = 1.0  # Inlet velocity
 D = 0.1  # Diameter of the cylinder
@@ -12,3 +15,12 @@ mu = rho * U_in * D / Re  # Dynamic viscosity
 nu = mu / rho  # Kinematic viscosity
 
 # Create computational domain and mesh
+Entry_length = 0.5  # Length of the domain
+Exit_length = 2.0  # Length of the domain
+height = 1.0
+domain = Rectangle(Point(-Entry_length, -height/2.0), Point(Exit_length, height/2.0)) - Circle(Point(0, 0), D / 2)
+mesh = generate_mesh(domain, 64)
+
+# show the mesh (Optonall)
+plot(mesh)
+plt.show()
